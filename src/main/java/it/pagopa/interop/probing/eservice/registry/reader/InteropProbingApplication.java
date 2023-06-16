6de81f22be73597ba19dc4e473d533cc664c2ea7
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-06-16 11:38:15 +0200

    Added xray implementation v1

--- src/main/java/it/pagopa/interop/probing/eservice/registry/reader/InteropProbingApplication.java
+++ src/main/java/it/pagopa/interop/probing/eservice/registry/reader/InteropProbingApplication.java
@@ -1,6 +1,7 @@
 package it.pagopa.interop.probing.eservice.registry.reader;
 
 import java.util.List;
+import com.amazonaws.xray.AWSXRay;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import it.pagopa.interop.probing.eservice.registry.reader.config.BaseModule;
@@ -11,21 +12,21 @@ import it.pagopa.interop.probing.eservice.registry.reader.config.mapping.mapper.
 import it.pagopa.interop.probing.eservice.registry.reader.dto.impl.EserviceDTO;
 import it.pagopa.interop.probing.eservice.registry.reader.producer.ServicesSend;
 import it.pagopa.interop.probing.eservice.registry.reader.service.BucketService;
+import it.pagopa.interop.probing.eservice.registry.reader.util.ProjectConstants;
 
 public class InteropProbingApplication {
-
 
   public static void main(String[] args) throws Exception {
     Injector injector = Guice.createInjector(new BaseModule(), new JacksonMapperConfig(),
         new PropertiesLoader(), new SqsConfig(), new BucketConfig());
-
     BucketService bucketService = injector.getInstance(BucketService.class);
     ServicesSend servicesSend = injector.getInstance(ServicesSend.class);
-
+    AWSXRay.beginSegment(ProjectConstants.APPLICATION_NAME);
     List<EserviceDTO> services = bucketService.readObject();
     for (EserviceDTO eservice : services) {
       servicesSend.sendMessage(eservice);
     }
+    AWSXRay.endSegment();
   }
 
 }
--- src/main/java/it/pagopa/interop/probing/eservice/registry/reader/service/BucketService.java
+++ src/main/java/it/pagopa/interop/probing/eservice/registry/reader/service/BucketService.java
@@ -1,10 +1,13 @@
 package it.pagopa.interop.probing.eservice.registry.reader.service;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
@@ -30,7 +33,13 @@ public class BucketService {
   private ObjectMapper mapper;
 
   public List<EserviceDTO> readObject() throws IOException {
-    S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucketName, bucketKey));
+    S3Object s3Object = new S3Object();
+    s3Object.setBucketName("prova");
+    s3Object.setKey("key");
+    File initialFile = new File(
+        "C:\\Users\\gdellorusso\\OneDrive - DXC Production\\Documents\\PagoPA\\E-service\\file.json");
+    InputStream targetStream = new FileInputStream(initialFile);
+    s3Object.setObjectContent(new S3ObjectInputStream(targetStream, null));
     return mapper.readValue(s3Object.getObjectContent(), new TypeReference<List<EserviceDTO>>() {});
   }
 
