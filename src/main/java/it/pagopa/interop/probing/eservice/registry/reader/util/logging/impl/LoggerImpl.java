811d9a041c474437c24e9e8635316614adf770da
EAD\lfortunato <luigi.fortunato@dxc.com>
2023-06-28 16:15:15 +0200

    Added trace id in the log

--- src/main/java/it/pagopa/interop/probing/eservice/registry/reader/util/logging/impl/LoggerImpl.java
+++ src/main/java/it/pagopa/interop/probing/eservice/registry/reader/util/logging/impl/LoggerImpl.java
@@ -1,10 +1,10 @@
 package it.pagopa.interop.probing.eservice.registry.reader.util.logging.impl;
 
-import it.pagopa.interop.probing.eservice.registry.reader.util.logging.Logger;
-import lombok.extern.slf4j.Slf4j;
-
 import java.net.URI;
 import java.util.UUID;
+import com.amazonaws.xray.AWSXRay;
+import it.pagopa.interop.probing.eservice.registry.reader.util.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LoggerImpl implements Logger {
@@ -13,7 +13,8 @@ public class LoggerImpl implements Logger {
   public void logMessagePushedToQueue(UUID eserviceId, UUID versionId, URI queueUrl,
       String queueGroupId) {
     log.info(
-        "e-service has been pushed in queue. eserviceId={}, versionId={}, queue={}, groupId={}",
-        eserviceId, versionId, queueUrl.toString(), queueGroupId);
+        "[TRACE_ID= {}] - e-service has been pushed in queue. eserviceId={}, versionId={}, queue={}, groupId={}",
+        AWSXRay.getCurrentSegment().getTraceId().toString(), eserviceId, versionId,
+        queueUrl.toString(), queueGroupId);
   }
 }
