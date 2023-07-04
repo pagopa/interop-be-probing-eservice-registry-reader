f4ce9f36cb09bd5cea86b977804355dfea29586a
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-07-04 15:43:37 +0200

    managed ContextMissing case

--- src/main/java/it/pagopa/interop/probing/eservice/registry/reader/config/aws/sqs/SqsConfig.java
+++ src/main/java/it/pagopa/interop/probing/eservice/registry/reader/config/aws/sqs/SqsConfig.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.handlers.TracingHandler;
+import com.amazonaws.xray.strategy.IgnoreErrorContextMissingStrategy;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
@@ -19,6 +20,7 @@ public class SqsConfig extends AbstractModule {
   @Provides
   @Singleton
   public AmazonSQSAsync provideAmazonSQSAsync() {
+    AWSXRay.getGlobalRecorder().setContextMissingStrategy(new IgnoreErrorContextMissingStrategy());
     return AmazonSQSAsyncClientBuilder.standard()
         .withCredentials(new DefaultAWSCredentialsProviderChain())
         .withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder())).build();
