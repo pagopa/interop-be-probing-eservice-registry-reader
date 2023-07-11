d8cfebf2a4eaf4ad2c919bfc152adf0bfde6f521
angelaantoniaceaa <107028078+angelaantoniaceaa@users.noreply.github.com>
2023-07-11 13:36:11 +0200

    Merge feature/IESP-235 (#17)
    Co-authored-by: acolucci <a.colucci@dxc.com>

--- src/main/java/it/pagopa/interop/probing/eservice/registry/reader/util/logging/LoggerLayout.java
+++ src/main/java/it/pagopa/interop/probing/eservice/registry/reader/util/logging/LoggerLayout.java
@@ -1,11 +1,8 @@
 package it.pagopa.interop.probing.eservice.registry.reader.util.logging;
 
 import java.sql.Timestamp;
-import java.util.UUID;
-import org.slf4j.MDC;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.LayoutBase;
-import it.pagopa.interop.probing.eservice.registry.reader.util.ProjectConstants;
 
 public class LoggerLayout extends LayoutBase<ILoggingEvent> {
 
@@ -17,16 +14,7 @@ public class LoggerLayout extends LayoutBase<ILoggingEvent> {
     sbuf.append(event.getLevel());
     sbuf.append(" [");
     sbuf.append(event.getLoggerName());
-    sbuf.append("]");
-    try {
-      sbuf.append(" - [CID=");
-      MDC.put(ProjectConstants.TRACE_ID_PLACEHOLDER, UUID.randomUUID().toString().toLowerCase());
-      sbuf.append(MDC.get(ProjectConstants.TRACE_ID_PLACEHOLDER));
-      sbuf.append("] - ");
-    } finally {
-      MDC.remove(ProjectConstants.TRACE_ID_PLACEHOLDER);
-    }
-
+    sbuf.append("] - ");
     sbuf.append(event.getFormattedMessage());
     sbuf.append("\n");
     return sbuf.toString();
